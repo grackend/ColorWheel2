ec641c4f57ca756cb2e0370d5f760ab25d3ed410
grackend <grackend@github.com>
2014-06-24 10:34:01 -0700

    Have some globals

--- ColorController.swift
+++ ColorController.swift
@@ -9,13 +9,17 @@
 import Foundation
 import UIKit
 
+// I feel like this is about to be way more complicated than it could be
+
+
+
 class ColorController : UIView {
     override func setNeedsLayout() {
         super.setNeedsLayout()
         // Do any additional setup after loading the view, typically from a nib.
         var colorArray: UIColor[] = [UIColor(), UIColor(), UIColor(), UIColor()]
         
-        let origColor = UIColor(red: 255, green: 0, blue: 0, alpha: 1)
+        let origColor = UIColor(red: 0.10196, green: 1, blue: 0.7764, alpha: 1)
         colorArray[0] = origColor
         
         //math stuff:
@@ -23,13 +27,19 @@ class ColorController : UIView {
         var greenVal: CMutablePointer<CGFloat> = nil
         var blueVal: CMutablePointer<CGFloat> = nil
         var alpha1: CMutablePointer<CGFloat> = nil
+                
+        var rgba = CGFloat[](count: 4, repeatedValue: 0.0)
+        origColor.getRed(&rgba[0], green: &rgba[1], blue: &rgba[2], alpha: &rgba[3])
         
-        var newColor = UIColor(CGColor: origColor.CGColor)
+        var MatchOne: UIColor = UIColor(red: rgba[0], green: rgba[2], blue: rgba[1], alpha: rgba[3])
+        var MatchTwo: UIColor = UIColor(red: rgba[2], green: rgba[1], blue: rgba[0], alpha: rgba[3])
+        var MatchThree: UIColor = UIColor(red: rgba[1], green: rgba[2], blue: rgba[0], alpha: rgba[3])
         
-        
-        origColor.getRed(redVal, green: greenVal, blue: blueVal, alpha: alpha1)
-        
-        //var matchOne: UIColor = UIColor(&redVal, &greenVal, &blueVal, &alpha1)
+        colorArray[1] = MatchOne
+        colorArray[2] = MatchTwo
+        colorArray[3] = MatchThree
+
+        println("red: \(rgba[0]), green: \(rgba[1]), blue: \(rgba[2]), alpha: \(rgba[3])")
         
         if self.accessibilityLabel == "ColorOriginal" {
             self.backgroundColor = colorArray[0]
---
+++ ColorWheel/ButtonController.swift
@@ -0,0 +1,43 @@
+//
+//  ButtonController.swift
+//  ColorWheel
+//
+//  Created by Grace Kendall on 6/24/14.
+//  Copyright (c) 2014 Grace Kendall. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var orange: Bool = false; var fuschia: Bool = false; var magenta: Bool = false
+var purple: Bool = false; var blue: Bool = false; var lightBlue: Bool = false
+var green: Bool = false; var yellowGreen: Bool = false; var yellow: Bool = false
+
+
+//(void)addTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents;
+class ButtonController: UIButton {
+    override func addTarget(target: AnyObject!, action: Selector, forControlEvents controlEvents: UIControlEvents){
+        if self.selected{
+            if self.accessibilityLabel == "Orange" {
+                orange = true
+            }else if self.accessibilityLabel == "Fuschia" {
+                fuschia = true
+            }else if self.accessibilityLabel == "Magenta" {
+                magenta = true
+            }else if self.accessibilityLabel == "Purple" {
+                purple = true
+            }else if self.accessibilityLabel == "Blue" {
+                blue = true
+            }else if self.accessibilityLabel == "LightBlue" {
+                lightBlue = true
+            }else if self.accessibilityLabel == "Green" {
+                green = true
+            }else if self.accessibilityLabel == "YellowGreen" {
+                yellowGreen = true
+            }else if self.accessibilityLabel == "Yellow" {
+                yellow = true
+            }
+        }
+        super.addTarget(target: UIButton, action: Selector, controlEvents: UIControlEvents)
+    }
+}
